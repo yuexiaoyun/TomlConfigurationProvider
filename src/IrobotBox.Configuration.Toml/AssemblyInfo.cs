08a92ad5ccae6b3c5b076f4ff216782ba7e1a4b4
岳小云 <sfreee@gmail.com>
2019-11-05 16:44:50 +0800

    init

---
+++ src/IrobotBox.Configuration.Toml/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("IrobotBox.Configuration.Tom ConfigurationProvider")]
+[assembly: AssemblyCompany("IrobotBox Technology Co., Ltd")]
+
+[assembly: AssemblyVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor
+)]
+
+[assembly: AssemblyFileVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.SemVer.Patch
+)]
+
+[assembly: AssemblyInformationalVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.SemVer.Patch + "-" +
+    ThisAssembly.Git.Branch + "+" +
+    ThisAssembly.Git.Commit
+)]
